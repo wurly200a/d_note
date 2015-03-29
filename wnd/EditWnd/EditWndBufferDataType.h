bd63f28badafdb2acbdadfbb8e6bb6ce8b27d9cf
yushi <yushi@dbae37e1-483b-0410-8081-86c1b21c2f05>
2015-03-29 08:28:35 +0000

    EditWndBufferDataType.h のヘッダコメント変更
    git-svn-id: http://mozuku/svn/editorRepo/tedd/trunk@175 dbae37e1-483b-0410-8081-86c1b21c2f05

--- wnd/EditWnd/EditWndBufferDataType.h
+++ wnd/EditWnd/EditWndBufferDataType.h
@@ -10,16 +10,101 @@ typedef struct tag_buffer_line_data
     TCHAR         data[]         ;
 } S_BUFF_LINE_DATA;
 
-S_BUFF_LINE_DATA *createBuffLineData( DWORD size, INT newLineCodeSize, TCHAR *dataPtr, DWORD lineNum, DWORD caretPos );
-S_BUFF_LINE_DATA *joinData( S_BUFF_LINE_DATA *data1Ptr, S_BUFF_LINE_DATA *data2Ptr );
+/********************************************************************************
+ * 内容  : 行データの生成
+ * 引数  : DWORD size          データサイズ(改行コード含む)
+ * 引数  : INT newLineCodeSize 改行コードサイズ
+ * 引数  : TCHAR *dataPtr      データ(ポインタ)
+ * 引数  : DWORD lineNum       行番号
+ * 引数  : DWORD caretPos      キャレット位置
+ * 戻り値: S_BUFF_LINE_DATA *
+ ***************************************/
+S_BUFF_LINE_DATA * createBuffLineData( DWORD size, INT newLineCodeSize, TCHAR *dataPtr, DWORD lineNum, DWORD caretPos );
+
+/********************************************************************************
+ * 内容  : 行データの結合
+ * 引数  : S_BUFF_LINE_DATA *data1Ptr (改行コードは削除される)
+ * 引数  : S_BUFF_LINE_DATA *data2Ptr
+ * 戻り値: S_BUFF_LINE_DATA *
+ ***************************************/
+S_BUFF_LINE_DATA * joinData( S_BUFF_LINE_DATA *data1Ptr, S_BUFF_LINE_DATA *data2Ptr );
+
+/********************************************************************************
+ * 内容  : 行データの分割
+ * 引数  : S_BUFF_LINE_DATA *dataPtr  分割するデータ
+ * 引数  : S_BUFF_LINE_DATA **new1Ptr 分割後のデータ1(のポインタ) (メモリ確保する)
+ * 引数  : S_BUFF_LINE_DATA **new2Ptr 分割後のデータ2(のポインタ) (メモリ確保する)
+ * 戻り値: なし
+ ***************************************/
 void divideData( S_BUFF_LINE_DATA *dataPtr, S_BUFF_LINE_DATA **new1PtrPtr, S_BUFF_LINE_DATA **new2PtrPtr );
-S_BUFF_LINE_DATA *shortenData( S_BUFF_LINE_DATA *dataPtr, DWORD size );
+
+/********************************************************************************
+ * 内容  : 行データを短くする
+ * 引数  : S_BUFF_LINE_DATA *dataPtr
+ * 引数  : DWORD size
+ * 戻り値: S_BUFF_LINE_DATA *
+ ***************************************/
+S_BUFF_LINE_DATA * shortenData( S_BUFF_LINE_DATA *dataPtr, DWORD size );
+
+/********************************************************************************
+ * 内容  : 行番号の更新
+ * 引数  : S_BUFF_LINE_DATA *dataPtr
+ * 戻り値: なし
+ ***************************************/
 void updateLineNum( S_BUFF_LINE_DATA *dataPtr );
+
+/********************************************************************************
+ * 内容  : 行データを追加する
+ * 引数  : S_LIST_HEADER **topPtrPtr 先頭データをつなぐポインタ(のポインタ)
+ * 引数  : S_LIST_HEADER **topPtrPtr 最終データをつなぐポインタ(のポインタ)
+ * 引数  : S_LIST_HEADER *dataPtr つなぐデータ
+ * 戻り値: なし
+ ***************************************/
 void addLineData( S_BUFF_LINE_DATA **topPtrPtr, S_BUFF_LINE_DATA **endPtrPtr, S_BUFF_LINE_DATA *dataPtr );
+
+/********************************************************************************
+ * 内容  : 行データを削除する
+ * 引数  : S_BUFF_LINE_DATA **topPtrPtr 先頭データがつないであるポインタ(のポインタ)
+ * 引数  : S_BUFF_LINE_DATA **endPtrPtr 最終データをつないであるポインタ(のポインタ)
+ * 引数  : S_BUFF_LINE_DATA *dataPtr    削除するデータ
+ * 戻り値: S_BUFF_LINE_DATA *           削除した次のデータ
+ ***************************************/
 void removeLineData( S_BUFF_LINE_DATA **topPtrPtr, S_BUFF_LINE_DATA **endPtrPtr, S_BUFF_LINE_DATA *dataPtr );
+
+/********************************************************************************
+ * 内容  : 行データの挿入
+ * 引数  : S_BUFF_LINE_DATA **topPtrPtr       先頭データをつなぐポインタ(のポインタ)
+ * 引数  : S_BUFF_LINE_DATA **endPtrPtr       最終データをつなぐポインタ(のポインタ)
+ * 引数  : S_BUFF_LINE_DATA *nowPtr           挿入位置
+ * 引数  : S_BUFF_LINE_DATA **insertTopPtrPtr
+ * 引数  : S_BUFF_LINE_DATA **insertEndPtrPtr
+ * 戻り値: void
+ ***************************************/
 void insertLineData( S_BUFF_LINE_DATA **topPtrPtr, S_BUFF_LINE_DATA **endPtrPtr, S_BUFF_LINE_DATA *nowPtr, S_BUFF_LINE_DATA **insertTopPtrPtr, S_BUFF_LINE_DATA **insertEndPtrPtr );
-S_BUFF_LINE_DATA *replaceLineData( S_BUFF_LINE_DATA **topPtrPtr, S_BUFF_LINE_DATA **endPtrPtr, S_BUFF_LINE_DATA *nowPtr, S_BUFF_LINE_DATA *dataPtr );
+
+/********************************************************************************
+ * 内容  : 行データを置き換える
+ * 引数  : S_BUFF_LINE_DATA **topPtrPtr 先頭データをつなぐポインタ(のポインタ)
+ * 引数  : S_BUFF_LINE_DATA **endPtrPtr 最終データをつなぐポインタ(のポインタ)
+ * 引数  : S_BUFF_LINE_DATA *nowPtr     置き換え対象のデータ
+ * 引数  : S_BUFF_LINE_DATA *dataPtr    置き換えるデータ
+ * 戻り値: 置き換えたデータ
+ ***************************************/
+S_BUFF_LINE_DATA * replaceLineData( S_BUFF_LINE_DATA **topPtrPtr, S_BUFF_LINE_DATA **endPtrPtr, S_BUFF_LINE_DATA *nowPtr, S_BUFF_LINE_DATA *dataPtr );
+
+/********************************************************************************
+ * 内容  : バッファデータのクリア
+ * 引数  : S_BUFF_LINE_DATA **topPtrPtr 先頭データをつなぐポインタ(のポインタ)
+ * 引数  : S_BUFF_LINE_DATA **endPtrPtr 最終データをつなぐポインタ(のポインタ)
+ * 戻り値: なし
+ ***************************************/
 void clearBuffLineData( S_BUFF_LINE_DATA **topPtrPtr, S_BUFF_LINE_DATA **endPtrPtr );
+
+/********************************************************************************
+ * 内容  : 行データの解放
+ * 引数  : S_BUFF_LINE_DATA *
+ * 戻り値: なし
+ ***************************************/
 void destroyBuffLineData( S_BUFF_LINE_DATA *dataPtr );
 
 #define EDITWND_BUFFER_DATA_TYPE_H
